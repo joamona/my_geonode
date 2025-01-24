16eea282b6ba063a6c2ffe153b55b40b96a7396a
joamona <joamona@cgf.upv.es>
2025-01-24 10:39:58 +0100

    m

--- clear.sh
+++ clear.sh
@@ -1,13 +1,21 @@
-#/bin/bash
-docker compose down
-docker volume rm my_geonode-backup-restore
-docker volume rm my_geonode-data
-docker volume rm my_geonode-dbbackups
-docker volume rm my_geonode-dbdata
-docker volume rm my_geonode-gsdatadir
-docker volume rm my_geonode-nginxcerts
-docker volume rm my_geonode-nginxconfd
-docker volume rm my_geonode-rabbitmq
-docker volume rm my_geonode-statics
-docker volume rm my_geonode-tmp
-docker volume rm my_geonode_jenkins_data
+sudo docker compose down
+sudo docker volume rm my_geonode-backup-restore
+sudo docker volume rm my_geonode-data
+sudo docker volume rm my_geonode-dbbackups
+sudo docker volume rm my_geonode-dbdata
+sudo docker volume rm my_geonode-gsdatadir
+sudo docker volume rm my_geonode-nginxcerts
+sudo docker volume rm my_geonode-nginxconfd
+sudo docker volume rm my_geonode-rabbitmq
+sudo docker volume rm my_geonode-statics
+sudo docker volume rm my_geonode-tmp
+sudo docker volume rm my_geonode_jenkins_data
+sudo docker rmi my_geonode_django
+sudo docker rmi geonode/nginx
+sudo docker rmi geonode/letsencrypt
+sudo docker rmi geonode/geoserver
+sudo docker rmi geonode/geoserver_data
+sudo docker rmi geonode/postgis
+sudo docker rmi rabbitmq
+sudo docker rmi jenkins/jenkins
+
